10d212540bca84955fd11e88444ff300562f88d7
Baxter, Benjamin <benjamin.baxter@kroger.com>
2016-10-19 18:26:13 -0400

    Added more comments

--- src/main/java/edu/nku/csc364/list/SimpleLinkedList.java
+++ src/main/java/edu/nku/csc364/list/SimpleLinkedList.java
@@ -1,12 +1,21 @@
 package edu.nku.csc364.list;
 
 /**
+ * A simple implementation of a linked list.
  *
+ * This list contains a reference to the head
+ * and performs all operations relative to
+ * knowing the head value.
  */
 public class SimpleLinkedList<T> {
 
     private Node<T> head;
 
+    /**
+     * This insert operation will always add the new node to
+     * the end of linked list
+     * @param data
+     */
     public void insert(T data) {
 
         Node<T> node = new Node<>(data);
