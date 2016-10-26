9ce96242f5f42cf82638fb1ba9e281f156d43b69
Baxter, Benjamin <benjamin.baxter@kroger.com>
2016-10-26 18:44:09 -0400

    Converted from Stack<T> to Stack<Integer>

--- src/test/java/edu/nku/csc364/stack/_1MinStackExtensionsTest.java
+++ src/test/java/edu/nku/csc364/stack/_1MinStackExtensionsTest.java
@@ -20,7 +20,7 @@ import static com.google.common.truth.Truth.assertThat;
  */
 public class _1MinStackExtensionsTest {
 
-    StackExtensions<Integer> stack;
+    StackExtensions stack;
 
     @Before
     public void setup() {
